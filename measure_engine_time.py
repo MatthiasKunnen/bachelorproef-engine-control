e2e4a785dd0bf1316da2f3ca3200e6efd76613c9
Matthias Kunnen <matthias.kunnen@gmail.com>
2017-06-12 03:50:54 +0200

    Added script to measure engine run time

---
+++ measure_engine_time.py
@@ -0,0 +1,20 @@
+import time
+from default_engine_control import get_engine_control
+from engine_control import EngineControl
+from sys import stdin
+from sys import stdout
+
+stdout.write("Enter {0} to start clockwise test and {1} to start counterclockwise: "
+             .format(EngineControl.State.CLOCKWISE.value,
+                     EngineControl.State.COUNTERCLOCKWISE.value))
+desiredState = int(stdin.readline())
+
+engineControl = get_engine_control()
+engineControl.set_state(desiredState)
+start = time.time()
+stdout.write("Hit enter to stop")
+stdin.readline()
+end = time.time()
+engineControl.set_state(EngineControl.State.OFF)
+
+print("The elapsed time in seconds is: {0}".format(end - start))
