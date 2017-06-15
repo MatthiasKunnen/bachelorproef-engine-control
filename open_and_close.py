d9f009bb658bac903f8d3a8d530b24321f48b1a1
Matthias Kunnen <matthias.kunnen@gmail.com>
2017-06-15 06:41:23 +0200

    Made open_and_close importable and self sufficient
    The script can now be used in other scripts but can still be called on
    its own.

--- open_and_close.py
+++ open_and_close.py
@@ -1,21 +1,15 @@
-import argparse as ap
 import time
 from default_engine_control import get_engine_control
 from engine_control import EngineControl
 
-parser = ap.ArgumentParser(description='Run engine clockwise. Wait. Run the '
-                                       'engine counterclockwise')
-parser.add_argument('-t', '--time', type=float, default=1,
-                    help='Time in seconds that the engine needs to run to '
-                         'travel in one direction.')
-parser.add_argument('-d', '--delay', type=float, default=3,
-                    help='Time in seconds until starting the travel in the '
-                         'opposite direction')
-args, leftovers = parser.parse_known_args()
 
-runTime = args.time
-keepOpen = args.delay
-engineControl = get_engine_control()
+def extend_parser(parser):
+    parser.add_argument('-t', '--time', type=float, default=1,
+                        help='Time in seconds that the engine needs to run to '
+                             'travel in one direction.')
+    parser.add_argument('-d', '--delay', type=float, default=3,
+                        help='Time in seconds until starting the travel in the '
+                             'opposite direction')
 
 
 def open_and_close(engine_control, run_time, delay):
@@ -34,4 +28,14 @@ def open_and_close(engine_control, run_time, delay):
     engine_control.set_state(EngineControl.State.OFF)
 
 
-open_and_close(engineControl, runTime, keepOpen)
+if __name__ == '__main__':
+    import argparse as ap
+
+    p = ap.ArgumentParser(description='Run engine clockwise. Wait. Run the '
+                                      'engine counterclockwise')
+    extend_parser(p)
+    args, leftovers = p.parse_known_args()
+    runTime = args.time
+    keepOpen = args.delay
+    engineControl = get_engine_control()
+    open_and_close(engineControl, runTime, keepOpen)
